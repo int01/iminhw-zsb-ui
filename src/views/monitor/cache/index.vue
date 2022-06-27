76dc7084f6141b7158b85a2cfbac75a91bcb8440
凉伊 <iminhw@qq.com>
2022-06-27 21:34:39 +0800

    增加redis缓存，同步ruoyi大佬最新版

--- src/views/monitor/cache/index.vue
+++ src/views/monitor/cache/index.vue
@@ -8,34 +8,112 @@
             <table cellspacing="0" style="width: 100%">
               <tbody>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">Redis版本</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.redis_version }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">运行模式</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.redis_mode == "standalone" ? "单机" : "集群" }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">端口</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.tcp_port }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">客户端数</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.connected_clients }}</div></td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">Redis版本</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.redis_version }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">运行模式</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{
+                        cache.info.redis_mode == "standalone" ? "单机" : "集群"
+                      }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">端口</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.tcp_port }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">客户端数</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.connected_clients }}
+                    </div>
+                  </td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">运行时间(天)</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.uptime_in_days }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">使用内存</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.used_memory_human }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">使用CPU</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ parseFloat(cache.info.used_cpu_user_children).toFixed(2) }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">内存配置</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.maxmemory_human }}</div></td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">运行时间(天)</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.uptime_in_days }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">使用内存</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.used_memory_human }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">使用CPU</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{
+                        parseFloat(cache.info.used_cpu_user_children).toFixed(2)
+                      }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">内存配置</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.maxmemory_human }}
+                    </div>
+                  </td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">AOF是否开启</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.aof_enabled == "0" ? "否" : "是" }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">RDB是否成功</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.rdb_last_bgsave_status }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">Key数量</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.dbSize">{{ cache.dbSize }} </div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">网络入口/出口</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell" v-if="cache.info">{{ cache.info.instantaneous_input_kbps }}kps/{{cache.info.instantaneous_output_kbps}}kps</div></td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">AOF是否开启</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.aof_enabled == "0" ? "否" : "是" }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">RDB是否成功</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.rdb_last_bgsave_status }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">Key数量</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.dbSize">
+                      {{ cache.dbSize }}
+                    </div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell">网络入口/出口</div>
+                  </td>
+                  <td class="el-table__cell is-leaf">
+                    <div class="cell" v-if="cache.info">
+                      {{ cache.info.instantaneous_input_kbps }}kps/{{
+                        cache.info.instantaneous_output_kbps
+                      }}kps
+                    </div>
+                  </td>
                 </tr>
               </tbody>
             </table>
@@ -66,9 +144,9 @@
   </div>
 </template>
 
-<script setup>
-import { getCache } from '@/api/monitor/cache';
-import * as echarts from 'echarts';
+<script setup name="Cache">
+import { getCache } from "@/api/monitor/cache";
+import * as echarts from "echarts";
 
 const cache = ref([]);
 const commandstats = ref(null);
@@ -77,7 +155,7 @@ const { proxy } = getCurrentInstance();
 
 function getList() {
   proxy.$modal.loading("正在加载缓存监控数据，请稍候！");
-  getCache().then(response => {
+  getCache().then((response) => {
     proxy.$modal.closeLoading();
     cache.value = response.data;
 
@@ -85,7 +163,7 @@ function getList() {
     commandstatsIntance.setOption({
       tooltip: {
         trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
+        formatter: "{a} <br/>{b} : {c} ({d}%)",
       },
       series: [
         {
@@ -96,15 +174,15 @@ function getList() {
           center: ["50%", "38%"],
           data: response.data.commandStats,
           animationEasing: "cubicInOut",
-          animationDuration: 1000
-        }
+          animationDuration: 1000,
+        },
       ],
     });
 
     const usedmemoryInstance = echarts.init(usedmemory.value, "macarons");
     usedmemoryInstance.setOption({
       tooltip: {
-        formatter: "{b} <br/>{a} : " + cache.value.info.used_memory_human
+        formatter: "{b} <br/>{a} : " + cache.value.info.used_memory_human,
       },
       series: [
         {
@@ -113,18 +191,18 @@ function getList() {
           min: 0,
           max: 1000,
           detail: {
-            formatter: cache.value.info.used_memory_human
+            formatter: cache.value.info.used_memory_human,
           },
           data: [
             {
               value: parseFloat(cache.value.info.used_memory_human),
-              name: "内存消耗"
-            }
-          ]
-        }
+              name: "内存消耗",
+            },
+          ],
+        },
       ],
-    })
-  })
+    });
+  });
 }
 
 getList();
