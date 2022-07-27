c7b496c4299805bf3209cb85c5b47df53c1b5aeb
凉伊 <iminhw@qq.com>
2022-07-27 21:50:57 +0800

    修正密钥

--- src/utils/jsencrypt.js
+++ src/utils/jsencrypt.js
@@ -3,38 +3,29 @@ import JSEncrypt from "jsencrypt/bin/jsencrypt.min";
 // 密钥对生成 http://web.chacuo.net/netrsakeypair
 
 const publicKey =
-  "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDT+E7gTmiD6wOTPaTl5sK5LJbK\n" +
-  "h+ult/9SGeVddk7TvhDH498vTFYosSWdoIvEtCI2jiPN1saT5u2bK6RBY2H5ZxpG\n" +
-  "jbUJiC7inv2ilW8OoTTUqCK1mnv397I8rus8oZZzmdzM/rXDog4XAlWGXfWE2qhE\n" +
-  "LDEmMuPVA/lJxUsE1QIDAQAB";
+    "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJvU8sAWn096XtXD1Z3VVvNjsuv8V8Hh\n" +
+    "mPa7WZ0d1/yH5jCqRzrD7xMf7WjB4vfKHJvF11tRCwRoLSeYuekPucsCAwEAAQ==";
 
 const privateKey =
-  "MIICxjBABgkqhkiG9w0BBQ0wMzAbBgkqhkiG9w0BBQwwDgQIbY4b2eZu3FMCAggA\n" +
-  "MBQGCCqGSIb3DQMHBAic76ElGxOmJgSCAoC4EnJ8bRJWb6+o84N0crEBZkgH86eC\n" +
-  "KqqvPcO4RVNLA0awuFLnA8UoVCrd1GPPOCeCrP4ReMRECSVPdXRcSlO75Av+p9J+\n" +
-  "3D+/lfldpCJ9p+X9Vd1o+20ZLv4Pr/MwqOr0VOMQmDEWJSUzoJuvxvc5bfyMiz/2\n" +
-  "qaw4022Hup+akPRbP1O7W+HS7/SBmoLoVZN7a//psObetKllUMylJzpE2iXd3jQg\n" +
-  "v0CItRAI7HTsg8n9KWDI/jZCze3/jN8UpRrFeVzUJuOXhV5TG8AvqxIvHU28DdRu\n" +
-  "L2riYNYf02IJlIQBjQPxK2kDfLPxLH4Dvj4E/HHdH6NLaVe6yU5x/aQtCBZUevm2\n" +
-  "gM9K5iVCZ2stQnkaokPgaOpvlHqfIsBJ42jPNAVkt7W9tSFUeVKe/oqe/L/vSlfc\n" +
-  "r8Ku0DyzYUgJfjShEe3kocQ0bpF6jhdXYBkZ7G8jetQYmSj8r8P+t18NqRhnCcL/\n" +
-  "UVKJtpCgU9KSu2R8OjZOC43ziEbB28pwTcSTdZT9RHXeRbT2kDR86E00NjKYCzs6\n" +
-  "/hVpHHUedz2tbd5PidH+LlexVc4nEVsQyFtyWAH7/UZ/Q76Y261OeJBWnRNAw9Fv\n" +
-  "bccgXhwHRQ4TvoeO74tXoUo3voKh1/1l1gWz56DKqpiPmL/zyHhBGoYyRZXc7pu2\n" +
-  "nmPoGQn3viMGPZEhEyrBOaK48rDzeHXd3VGinrhLaMj7rFO9TBpdS3n7jrJQ4kJy\n" +
-  "1umj2zUJPoUB+/pUrxOqIxd9UswxSkhmTPUaJU8eup1AKJr7/Ym3zDCyiecRf4j4\n" +
-  "XM726azZQXS7nHm1ohwg6Y+/reblPEZu1jkWD1+sAaEyf8hD3R8qzdBu";
+    "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAm9TywBafT3pe1cPV\n" +
+    "ndVW82Oy6/xXweGY9rtZnR3X/IfmMKpHOsPvEx/taMHi98ocm8XXW1ELBGgtJ5i5\n" +
+    "6Q+5ywIDAQABAkAcrj7Xd9JucFlQArEoUYGlXi4qHNGSD5F1p9nAfyOniJobak97\n" +
+    "Jn8b4WCUDg+iA+/1pU4Ap/AulDmOZiwkNZtxAiEAzPK25C7vxP59VjhJw2DaT3r3\n" +
+    "RWRG42qHFx5kCmTDmy0CIQDCpiZumnPNqiq1otntwTkxe4hTx8wJUzz7UsXYFp5j\n" +
+    "1wIget8NDTKip00X4agveQZI/BuxwZWD9nX4dcDPo3eT2iUCIByZRtpHTS5xixbm\n" +
+    "13gsRJf1m4EbwOm51iLi9XibUig/AiEAsw5A9Z/8l7iMlYxDH++V8KAk4/L6qlN2\n" +
+    "8QXkEHYqcJY=";
 
 // 加密
 export function encrypt(txt) {
-  const encryptor = new JSEncrypt();
-  encryptor.setPublicKey(publicKey); // 设置公钥
-  return encryptor.encrypt(txt); // 对数据进行加密
+    const encryptor = new JSEncrypt();
+    encryptor.setPublicKey(publicKey); // 设置公钥
+    return encryptor.encrypt(txt); // 对数据进行加密
 }
 
 // 解密
 export function decrypt(txt) {
-  const encryptor = new JSEncrypt();
-  encryptor.setPrivateKey(privateKey); // 设置私钥
-  return encryptor.decrypt(txt); // 对数据进行解密
+    const encryptor = new JSEncrypt();
+    encryptor.setPrivateKey(privateKey); // 设置私钥
+    return encryptor.decrypt(txt); // 对数据进行解密
 }
