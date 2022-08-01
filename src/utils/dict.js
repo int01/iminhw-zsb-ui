5af9983d04d480e71b0ce8ce4b39b75a126100dc
凉伊 <iminhw@qq.com>
2022-08-01 23:51:20 +0800

    优化字典数据使用store存取

--- src/utils/dict.js
+++ src/utils/dict.js
@@ -1,4 +1,5 @@
 import { getDicts } from "@/api/system/dict/data";
+import useDictStore from "@/store/modules/dict";
 
 /**
  * 获取字典数据
@@ -6,28 +7,34 @@ import { getDicts } from "@/api/system/dict/data";
 export function useDict(...args) {
   const res = ref({});
   return (() => {
-    args.forEach((d, index) => {
-      res.value[d] = [];
-      getDicts(d).then((resp) => {
-        res.value[d] = resp.data.map((p) => ({
-          label: p.dictLabel,
-          value: p.dictValue,
-          elTagType: p.listClass,
-          elTagClass: p.cssClass,
-        }));
-      });
+    args.forEach((dictType, index) => {
+      res.value[dictType] = [];
+      const dicts = useDictStore().getDict(dictType);
+      if (dicts) {
+        res.value[dictType] = dicts;
+      } else {
+        getDicts(dictType).then((resp) => {
+          res.value[dictType] = resp.data.map((p) => ({
+            label: p.dictLabel,
+            value: p.dictValue,
+            elTagType: p.listClass,
+            elTagClass: p.cssClass,
+          }));
+          useDictStore().setDict(dictType, res.value[dictType]);
+        });
+      }
     });
     return toRefs(res.value);
   })();
 }
 
 export function dictTagFun(options, key) {
-  if(key == null) {
+  if (key == null) {
     return "";
   }
   const items = options.value;
   for (const index in items) {
-    if(items[index].value == key) {
+    if (items[index].value == key) {
       return items[index].label;
     }
   }
